1a53e4566804a18855fc7713b3a26cf2d6f76f6d
gnanaprakash653@gmail.com <gnanaprakash653@gmail.com>
2025-06-29 19:52:05 +0530

    Created an Exception handler Created an Exception handler that handles all the exceptions that is being thrown

---
+++ Services/AuthService/src/main/java/com/example/AuthService/exception/AuthExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.example.AuthService.exception;
+
+import com.example.AuthService.domain.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AuthExceptionHandler {
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Response> handleUserAlreadyExists(UserAlreadyExistsException ex) {
+        return createErrorResponse(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Response> handleUserNotFound(UserNotFoundException ex) {
+        return createErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<Response> handleInvalidCredentials(InvalidCredentialsException ex) {
+        return createErrorResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<Response> handlePasswordMismatch(PasswordMismatchException ex) {
+        return createErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<Response> createErrorResponse(String message, HttpStatus status) {
+        Response response = new Response();
+        response.setError(message);
+        return ResponseEntity.status(status).body(response);
+    }
+}
