9a36e43a8ac2846b22478e7066f7414b51793188
Mohanapriya K <mohanapriyak305@gmail.com>
2025-06-29 11:36:34 +0530

    Movie service - Created Movie Repository

---
+++ Services/MovieService/src/main/java/com/example/MovieService/repository/MovieRepository.java
@@ -0,0 +1,16 @@
+package com.example.MovieService.repository;
+
+import com.example.MovieService.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<User,String > {
+
+    Optional<User> findByEmail(String email);
+    List<User> findAllByEmail(String email);
+
+}
