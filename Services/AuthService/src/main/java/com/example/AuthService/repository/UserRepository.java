9e6351a3df537e9f165108b958fd9192da5a9e1c
gnanaprakash653@gmail.com <gnanaprakash653@gmail.com>
2025-06-29 13:40:37 +0530

    Updates Added findByUsername and findByEmail Removed findByEmailAndPassword Removed the InvalidCredentials exception And Added UserAlreadyExists Exception

--- Services/AuthService/src/main/java/com/example/AuthService/repository/UserRepository.java
+++ Services/AuthService/src/main/java/com/example/AuthService/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.example.AuthService.repository;
 
 import com.example.AuthService.domain.User;
-import com.example.AuthService.exception.InvalidCredentialsException;
+import com.example.AuthService.exception.UserAlreadyExistsException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByEmailAndPassword(String mail, String password) throws InvalidCredentialsException;
+    Optional<User> findByUsername(String username) throws UserAlreadyExistsException;
+    Optional<User> findByEmail(String email) throws UserAlreadyExistsException;
 }
