0ca8b8418a5c63564c824694b393d9f042136790
gnanaprakash653@gmail.com <gnanaprakash653@gmail.com>
2025-06-29 19:55:32 +0530

    created error field

--- Services/AuthService/src/main/java/com/example/AuthService/domain/Response.java
+++ Services/AuthService/src/main/java/com/example/AuthService/domain/Response.java
@@ -1,17 +1,22 @@
 package com.example.AuthService.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Response {
     private String token;
     private String message;
     private User user;
+    private String error;
 
     public Response() {
     }
 
-    public Response(String token, String message, User user) {
+    public Response(String token, String message, User user, String error) {
         this.token = token;
         this.message = message;
         this.user = user;
+        this.error = error;
     }
 
     public String getToken() {
@@ -36,5 +41,13 @@ public class Response {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
     }
 }
