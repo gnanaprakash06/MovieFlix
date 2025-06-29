8a0de8ff9839cd4bb44fc01c0f04430abcda97ee
gnanaprakash653@gmail.com <gnanaprakash653@gmail.com>
2025-06-29 13:39:32 +0530

    Added few things A no-arg constructor Annotations on the field Getter and setter for id, username, phone number Updated the toString() method

--- Services/AuthService/src/main/java/com/example/AuthService/domain/User.java
+++ Services/AuthService/src/main/java/com/example/AuthService/domain/User.java
@@ -1,15 +1,47 @@
 package com.example.AuthService.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
     private String password;
 
-    public User(String email, String password) {
+    public User() {
+    }
+
+    public User(String username, String email, String phoneNumber, String password) {
+        this.username = username;
         this.email = email;
+        this.phoneNumber = phoneNumber;
         this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getEmail() {
@@ -20,6 +52,7 @@ public class User {
         this.email = email;
     }
 
+
     public String getPassword() {
         return password;
     }
@@ -28,11 +61,35 @@ public class User {
         this.password = password;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     @Override
     public String toString() {
         return "User{" +
-                "email='" + email + '\'' +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
                 ", password='" + password + '\'' +
                 '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
     }
 }
