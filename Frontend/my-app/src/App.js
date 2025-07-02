2788d714623ce7375ff960df9f7e68738ddf524e
Mohanapriya K <mohanapriyak305@gmail.com>
2025-07-02 16:52:26 +0530

    Removed serparate forgot/reset password file

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import ForgotPassword from './components/ForgotPassword';
-import ResetPassword from './components/ResetPassword';
 import Footer from './components/Footer';
 import MovieService from './services/MovieService';
 import './App.css';
@@ -24,27 +22,7 @@ function App() {
         return <SignIn onNavigate={onNavigate} />;
       case 'signup':
         return <SignUp onNavigate={onNavigate} />;
-      case 'forgotPassword':
-        return (
-          <ForgotPassword
-            onOTPSent={(email) => {
-              setPasswordResetEmail(email);
-              setCurrentView('resetPassword');
-            }}
-            onBack={() => setCurrentView('login')}
-          />
-        );
-      case 'resetPassword':
-        return (
-          <ResetPassword
-            email={passwordResetEmail}
-            onSuccess={() => {
-              alert('Password reset successfully! Please login with your new password.');
-              setCurrentView('login');
-            }}
-            onBack={() => setCurrentView('forgotPassword')}
-          />
-        );
+
       case 'movieservice':
         return <MovieService userEmail={window.userEmail} onNavigate={onNavigate} />;
       default:
