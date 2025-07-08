496d021762513a3c067245183ae23b51cfc2de09
gnanaprakash653@gmail.com <gnanaprakash653@gmail.com>
2025-07-08 14:48:33 +0530

    Individual imports Changed import all to individual import

--- Services/AuthService/src/main/java/com/example/AuthService/domain/User.java
+++ Services/AuthService/src/main/java/com/example/AuthService/domain/User.java
@@ -1,7 +1,12 @@
 package com.example.AuthService.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
