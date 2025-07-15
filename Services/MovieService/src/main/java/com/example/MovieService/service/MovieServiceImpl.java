33238d814b83e80a2ec8c18668edaef24ebb3ba1
gnanaprakash653@gmail.com <gnanaprakash653@gmail.com>
2025-07-15 17:57:59 +0530

    application.properties Ellathayum application.properties la irunthu vara maari panite and rest template ah auto wire panite

--- Services/MovieService/src/main/java/com/example/MovieService/service/MovieServiceImpl.java
+++ Services/MovieService/src/main/java/com/example/MovieService/service/MovieServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.MovieService.feignClient.UserAuthClient;
 import com.example.MovieService.repository.MovieRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,9 +23,13 @@ import java.util.stream.Collectors;
 public class MovieServiceImpl implements MovieService {
     private final MovieRepository movieRepository;
     private final UserAuthClient userAuthClient;
-    private final RestTemplate restTemplate = new RestTemplate();
-    private static final String tmdbApiKey = "4833b1d3a2c00e56714bd2905095d5c8";
-    private static final String tmdbBaseUrl = "https://api.themoviedb.org/3";
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${tmdb.api.key}")
+    private static String tmdbApiKey;
+    @Value("${tmdb.base.url}")
+    private static String tmdbBaseUrl;
     private static final Logger logger = LoggerFactory.getLogger(MovieServiceImpl.class);
 
     public MovieServiceImpl(MovieRepository movieRepository, UserAuthClient userAuthClient) {
